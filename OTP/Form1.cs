4d549ff7f1068f465984bdfd610c136f941086c5
barbaraandrasi <andrasibarbara@yahoo.com>
2019-06-08 21:58:07 +0300

    first commit

---
+++ OTP/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OTP
+{
+    public partial class Form1 : Form
+    {
+        private readonly string NUMS = "0123456789";
+        private readonly int LENGTH = 5;
+        private readonly Random RANDOM;
+        
+        private OneTimePassword oneTimePassword;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            RANDOM = new Random();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            string message = "";
+            if (value == oneTimePassword.Value)
+            {
+                if (oneTimePassword.Valid)
+                {
+                    oneTimePassword.Valid = false;
+                    message = "Login succeed!";
+                }
+                else
+                {
+                    message = "Login failed: password expired!";               
+                }
+            }
+            else
+            {
+                message = "Login failed: wrong password!";
+            }
+            if (MessageBox.Show(message) == DialogResult.OK)
+            {
+                textBox1.Text = "";
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oneTimePassword = new OneTimePassword()
+            {
+                Value = generateValue(),
+                Valid = true
+            };
+            if (MessageBox.Show(oneTimePassword.Value) == DialogResult.OK)
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private string generateValue()
+        {
+            string value = "";
+            for (int i = 0; i < LENGTH; i++)
+            {
+                value += NUMS[RANDOM.Next(NUMS.Length)];
+            }
+            return value;
+        }
+
+    }
+}
---
+++ OTP/OneTimePassword.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTP
+{
+    class OneTimePassword
+    {
+        public string Value { get; set; }
+        public bool Valid { get; set; }
+    }
+}
